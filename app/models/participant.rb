b46fe8adbddd9fd79bcdb0f1477cc0494ef0148b
Seiya Bryant <seiyathesaint@gmail.com>
2015-06-14 18:34:16 +0900

    created associations

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,2 +1,7 @@
 class Participant < ActiveRecord::Base
+  belongs_to :course
+  # This shows that the participants belong to or are connected to teh courses.
+  validates :name, presence: true, length: { maximum: 20 }
+  # This doesn't allow the user to create a user without entering a name
+
 end
